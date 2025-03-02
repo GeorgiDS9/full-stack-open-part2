e71900d496e4b8652e86e0297477006156326ab8
georgiana <gchiriac2012@gmail.com>
2025-03-02 13:26:22 -0500

    Exercises 2.4-2.5

--- courseinfo-2/src/components/Course.jsx
+++ courseinfo-2/src/components/Course.jsx
@@ -2,18 +2,24 @@ import PropTypes from "prop-types";
 import Content from "./Content";
 import Total from "./Total";
 
-const Course = ({ course }) => {
+const Course = ({ courses }) => {
   return (
     <div>
-      <h1>{course.name}</h1>
-      <Content course={course} />
-      <Total course={course} />
+      {courses?.map((course) => {
+        return (
+          <div key={course.id}>
+            <h2>{course.name}</h2>
+            <Content course={course} />
+            <Total course={course} />
+          </div>
+        );
+      })}
     </div>
   );
 };
 
 Course.propTypes = {
-  course: PropTypes.object.isRequired,
+  courses: PropTypes.array.isRequired,
 };
 
 Course.displayName = "Course";
