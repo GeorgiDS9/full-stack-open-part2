3f7a030adf2d21190c59c1aca7ed9b678baf5f11
georgiana <gchiriac2012@gmail.com>
2025-03-02 17:12:38 -0500

    Exercise 2.6

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,21 +1,40 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([{ name: "Arto Hellas", id: "1" }]);
   const [newName, setNewName] = useState("");
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    const personObject = {
+      name: newName,
+      id: String(persons.length + 1),
+    };
+    setPersons(persons.concat(personObject)); // Alternative that does not mutate the array: setPersons([...persons, personObject])
+    setNewName("");
+  };
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
+      <ul>
+        {persons.map((person) => (
+          <li key={person.id}>{person.name}</li>
+        ))}
+      </ul>
     </div>
   );
 };
